ad152b79972a2197ef3bb317bd3f721cf97bcec3
yeomsungpil <present7256@gmail.com>
2023-04-08 10:03:34 +0900

    feat: 가장 가까운 같은 글자 실패

--- 프로그래머스.playground/Contents.swift
+++ 프로그래머스.playground/Contents.swift
@@ -1,24 +1,9 @@
 import Foundation
 
-func solution(_ food:[Int]) -> String {
-    
-    // for문의 인덱스를 1부터 시작
-    var b1: String = ""
-    
-    for i in 1..<food.count {
-        var a1 = 0
-        a1 = food[i]/2
-        if a1 > 0 {
-            for _ in 0..<a1 {
-                b1 += String(i)
-            }
-        }
-        
-    }
-    
-    
-    
-    return b1 + String("0") + b1.reversed()
+func solution(_ s:String) -> [Int] {
+   
+    return []
 }
-let result = solution([1, 3, 4, 6])
+let result = solution("foobar")
 print(result)
+
