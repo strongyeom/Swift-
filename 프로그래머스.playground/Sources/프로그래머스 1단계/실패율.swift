939a1a7f940f584187eaeaede0590a8bd9176756
yeomsungpil <present7256@gmail.com>
2023-04-29 17:25:17 +0900

    feat: 실패율 풀이 최종본

--- 프로그래머스.playground/Sources/프로그래머스 1단계/실패율.swift
+++ 프로그래머스.playground/Sources/프로그래머스 1단계/실패율.swift
@@ -1,26 +1,25 @@
 import Foundation
 
-func solution실패율(_ N:Int, _ stages:[Int]) -> [Int] {
-    // 스테이지 : 실패율
-    var tuple = [(Int, Double)]()
-   
-    // 실패율 계산 후 저장
-    for i in 1...N{
-        // 도달한 수
-        let a = stages.filter{$0 >= i}.count
-        // 클리어한 수
-        let b = a - stages.filter{$0 > i}.count
-        // 실패율
-        let failCount = Double(b) / Double(a)
+func solution(_ N:Int, _ stages:[Int]) -> [Int] {
+        var tuple = [(Int, Double)]() // [스테이지: 실패율]
+        var player = stages.count
+        
+        for i in 1...N {
+            var current = 0
+            for j in 0..<stages.count {
+                if stages[j] == i {
+                    current += 1
+                }
+            }
+            
+            player -= current
+            
+            // 딕셔너리로 묶어서 담자
+            var ratio = Double(current) / Double(player)
+            tuple.append((i, ratio))
+        }
 
-        tuple.append(i, failCount)
-        print(tuple)
+        tuple = tuple.sorted(by: {$0.1 > $1.1 })
+        
+        return tuple.map{ $0.0 }
     }
-
-    // 실패율이 높은 순서대로 내림차순 숫자 등록, 실패율이 같으면 오름차순
-    tuple = tuple.sorted(by: {$0.1 > $1.1 })
-    
-    return tuple.map{ $0.0 }
-}
-
-
