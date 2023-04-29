abe569054d660eca6682b03433da0170ee855aa3
yeomsungpil <present7256@gmail.com>
2023-04-29 18:17:08 +0900

    feat: 다트게임 수정본

---
+++ 프로그래머스.playground/Sources/프로그래머스 1단계/1차 다트게임.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+func solution다트게임(_ dartResult:String) -> Int {
+    
+    var numbers = dartResult.split(whereSeparator: { $0.isLetter || $0 == "*" || $0 == "#"})
+    var letters = dartResult.split(whereSeparator: { $0.isNumber })
+
+    print (numbers)
+    print (letters)
+    
+    var answer = numbers.map{ Int($0)! }
+    
+    print(answer)
+    
+    for (index, element) in letters.enumerated() {
+        print("letter:", element)
+        for c in element {
+            print(c)
+            switch c {
+                case "D":
+                    answer[index] = answer[index] * answer[index]
+                case "T":
+                    answer[index] = answer[index] * answer[index] * answer[index]
+                case "*":
+                    if (index != 0) {
+                        answer[index] *= 2
+                        answer[index - 1] *= 2
+                    } else {
+                        answer[index] *= 2
+                    }
+                case "#":
+                    answer[index] *= -1
+                default:
+                    break
+            }
+            print(answer)
+        }
+    }
+    
+    return answer.reduce(0, +)
+}
