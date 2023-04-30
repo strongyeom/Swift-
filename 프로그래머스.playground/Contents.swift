a54d28350b3873234c209604b3d59d64f957f275
yeomsungpil <present7256@gmail.com>
2023-04-30 13:02:15 +0900

    add: 카드 뭉치 풀이 추가

--- 프로그래머스.playground/Contents.swift
+++ 프로그래머스.playground/Contents.swift
@@ -1,44 +1,27 @@
 import Foundation
 
-func solution(_ dartResult:String) -> Int {
-    
-    var numbers = dartResult.split(whereSeparator: { $0.isLetter || $0 == "*" || $0 == "#"})
-    var letters = dartResult.split(whereSeparator: { $0.isNumber })
-
-    print (numbers)
-    print (letters)
-    
-    var answer = numbers.map{ Int($0)! }
-    
-    print(answer)
-    
-    for (index, element) in letters.enumerated() {
-        print("letter:", element)
-        for c in element {
-            print(c)
-            switch c {
-                case "D":
-                    answer[index] = answer[index] * answer[index]
-                case "T":
-                    answer[index] = answer[index] * answer[index] * answer[index]
-                case "*":
-                    if (index != 0) {
-                        answer[index] *= 2
-                        answer[index - 1] *= 2
-                    } else {
-                        answer[index] *= 2
-                    }
-                case "#":
-                    answer[index] *= -1
-                default:
-                    break
-            }
-            print(answer)
+func solution(_ cards1:[String], _ cards2:[String], _ goal:[String]) -> String {
+    var cards1 = cards1, cards2 = cards2
+ 
+    for s in goal{
+        if !cards1.isEmpty && cards1[0] == s {
+            cards1.removeFirst()
+            print(cards1)
+        }else if !cards2.isEmpty && cards2[0] == s {
+            cards2.removeFirst()
+            print(cards2)
+        }else{
+            return "No"
         }
     }
-    
-    return answer.reduce(0, +)
+    return "Yes"
 }
 
-let result = solution("1S2D*3T")
+let result = solution(["i", "water", "drink"], ["want", "to"], ["i", "want", "to", "drink", "water"])
 print(result)
+
+/*
+ i : ["drink", "water"] , ["want", "to"]
+ want: ["drink", "water"], ["to"]
+ to: ["drink", "water"], []
+ */
