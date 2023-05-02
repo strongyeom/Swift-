2e53e8a6b2b1ccc51bfa08b19d0189c1a5cbf36b
yeomsungpil <present7256@gmail.com>
2023-05-02 15:42:35 +0900

    add: 로또 최고 다른 풀이 추가

--- 프로그래머스.playground/Sources/프로그래머스 1단계/로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.swift
+++ 프로그래머스.playground/Sources/프로그래머스 1단계/로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.swift
@@ -49,3 +49,29 @@ func solution로또(_ lottos:[Int], _ win_nums:[Int]) -> [Int] {
     
     return banswer
 }
+
+/*
+ 비슷하지만 다른풀이
+ func solution(_ lottos:[Int], _ win_nums:[Int]) -> [Int] {
+     var min = lottos.filter{win_nums.contains($0)}.count
+     var max = lottos.filter{$0 == 0}.count + min
+     var result = [max,min]
+
+     for i in 0..<result.count {
+         if result[i] == 6 {
+             result[i] = 1
+         } else if result[i] == 5 {
+             result[i] = 2
+         } else if result[i] == 4 {
+             result[i] = 3
+         } else if result[i] == 3 {
+             result[i] = 4
+         } else if result[i] == 2 {
+             result[i] = 5
+         } else {
+             result[i] = 6
+         }
+     }
+     return result
+ }
+ */
