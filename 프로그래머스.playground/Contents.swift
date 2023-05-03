21cd1a993a674a80c45f90d4c9e0079a8b465b78
yeomsungpil <present7256@gmail.com>
2023-05-04 00:29:09 +0900

    feat: 에라토스테네스의 체 연습

--- 프로그래머스.playground/Contents.swift
+++ 프로그래머스.playground/Contents.swift
@@ -1,106 +1,34 @@
 import Foundation
 
-func solution(_ number: Int, _ limit: Int, _ power: Int) -> Int {
-    var result = 0
-    var divisorCount = Array(repeating: 0, count: number + 1)
-    print(divisorCount)
+func findPrime(_ number: Int) -> [Int] {
     
-    // 각 숫자의 약수 개수를 계산
-    for i in 1...number {
-        // i : 1 number: 10, by: i
-        for j in stride(from: i, through: number, by: i) {
-            divisorCount[j] += 1
-            print(divisorCount)
+    // count: number를 넣어봤습니다.
+    var isPrime = Array(repeating: true, count: number)
+    print(isPrime)
+    
+    // 소수를 담아줄 배열
+    var primes: [Int] = []
+    
+    for i in 0..<number {
+        
+        if isPrime[i] == true {
+            primes.append(i)
+            for j in stride(from: i*i, through: number, by: i) {
+                isPrime[j] = false
+            }
         }
     }
-    
-    // 각 숫자의 약수 개수에 따라 결과 계산
-    for i in 1...number {
-        if divisorCount[i] <= limit {
-            result += divisorCount[i]
-        } else {
-            result += power
-        }
-    }
-    
-    return result
+    return primes
 }
 
-let result = solution(10, 3, 2)
+let result = findPrime(20)
 print(result)
 
 
 
-var aa = 10
-
-for i in 1...aa {
-    for j in stride(from: i, through: aa, by: i) {
-        print(j)
-    }
-}
-
-/*
- 실패 케이스 시간 초과
- 1. 첫번째 방법
- func solution(_ number:Int, _ limit:Int, _ power:Int) -> Int {
-     
-     // 해당 number의 약수의 갯수를 담는 배열을 만들고
-     // 약수의 갯수가 limit을 넘어가지 않으면 배열의 모든 수를 더한다
-     // 만약 약수의 갯수가 limit을 넘어가면 power로 변환 해준다
-     
-     var countArray: [Int] = []
-     var result = 0
-     
-     for i in 1...number {
-         var count = 0
-         for j in 1...number {
-             if i%j == 0 {
-                 count += 1
-                 print(count)
-             }
-         }
-         countArray.append(count)
-         print(countArray)
-     }
-     
-     for k in countArray {
-         if k >= limit {
-             result += power
-         } else {
-             result += k
-         }
-     }
-     
-     print(countArray)
-     return result
- }
- 
- 2. 두번째 방법
- func solution(_ number:Int, _ limit:Int, _ power:Int) -> Int {
-     
-     // 해당 number의 약수의 갯수를 담는 배열을 만들고
-     // 약수의 갯수가 limit을 넘어가지 않으면 배열의 모든 수를 더한다
-     // 만약 약수의 갯수가 limit을 넘어가면 power로 변환 해준다
-     
-     var countArray: [Int] = []
-     var result = 0
-     
-     for i in 1...number {
-         var count = 0
-         for j in 1...number {
-             if i%j == 0 {
-                 count += 1
-             }
-         }
-         countArray.append(count)
-         
-         if countArray.last! <= limit {
-             result += countArray.last!
-         } else {
-             result += power
-         }
-     }
-     print(countArray)
-     return result
- }
- */
+// i가 2일때
+// [true, true, true, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false]
+// i가 3일때
+// [true, true, true, true, false, true, false, true, false, false, false, true, false, true, false, false, false, true, false, true, false]
+// i가 4일때
+// [true, true, true, true, false, true, false, true, false, false, false, true, false, true, false, false, false, true, false, true, false]
