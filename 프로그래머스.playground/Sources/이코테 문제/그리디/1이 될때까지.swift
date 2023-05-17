6dc66cfe66bb00feb0382119741c670d0e2448c4
yeomsungpil <present7256@gmail.com>
2023-05-17 22:26:09 +0900

    add: 이코테 1이 될 때까지 문제 풀이 추가

--- 프로그래머스.playground/Sources/이코테 문제/그리디/1이 될때까지.swift
+++ 프로그래머스.playground/Sources/이코테 문제/그리디/1이 될때까지.swift
@@ -22,3 +22,32 @@ func solution(_ n: Int, _ k: Int) -> Int {
     
     return 0
 }
+
+/*
+ 2023.05.17 풀이
+ func solution(_ N: Int, _ K: Int) -> Int {
+     
+     // 계산 될 N을 변수로 만들기
+     var countN: Int = N
+     
+     // 계산 횟수
+     var result: Int = 0
+     
+     while countN > 1 {
+         if countN%K == 0 {
+             countN = countN/K
+             result += 1
+             print("나누기:",result)
+         } else {
+             countN -= 1
+             result += 1
+             print("마이너스:",result)
+         }
+     }
+     
+     print(result)
+     
+     
+     return 0
+ }
+ */
