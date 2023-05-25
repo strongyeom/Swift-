76366240bcc255a85c48bd5c9b1f50debedf71ba
yeomsungpil <present7256@gmail.com>
2023-05-25 16:51:31 +0900

    add: 신고 결과 받기 문제 추가

--- 프로그래머스.playground/Contents.swift
+++ 프로그래머스.playground/Contents.swift
@@ -1,55 +1,134 @@
 import Foundation
 
-func solution(_ today:String, _ terms:[String], _ privacies:[String]) -> [Int] {
-    var todayTime = today.split(separator:".").map{Int($0)!}
-    var termsArr = terms
-    var priv = privacies
-    var dict = [String: Int]()
-    var result = [Int]()
+func solution(_ id_list:[String], _ report:[String], _ k:Int) -> [Int] {
     
-    for i in 0..<terms.count {
-        var arrs = terms[i].split(separator: " ").map{ String($0)}
-        dict[arrs[0]] = Int(arrs[1])
+    // 값을 더할 때 필요한 결과값
+    var count = Array(repeating: 0, count: id_list.count)
+    print("count",count)
+    
+    var idx: [String:Int] = [:]
+    // 해당 문자가 들어오면 count 인덱스 올려주기 위한 변수
+    for (index, value) in id_list.enumerated() {
+        idx[value] = index
     }
+    print("idx",idx)
     
-    for i in 0..<priv.count {
-        var arr = priv[i].split(separator: " ").map{ String($0)}
-        var date = arr[0].split(separator: ".").map{ Int($0)!}
-        var term = dict[arr[1]]!
+    // 신고당한 사람 key, 신고한 사람 value, 중복 값 제거를 위해 Set활용
+    var dic: [String:Set<String>] = [:]
+    
+    for i in report {
+        let components = i.components(separatedBy: " ")
+        // 신고 한 사람
+        let personSingoed = components[0]
+        // 신고 당한사람
+        let singo = components[1]
         
-        date[1] += term
-        if date[1] > 12 {
-            if date[1] % 12 == 0 {
-                date[0] = date[0] + date[1] / 12 - 1
-            } else {
-                date[0] = date[0] + date[1] / 12
-            }
-            while date[1] > 12 {
-                date[1] -= 12
+        if dic[singo] == nil {
+            dic[singo] = [personSingoed]
+        } else {
+            dic[singo]?.insert(personSingoed)
+        }
+    }
+    print("dic",dic)
+    
+    for j in dic {
+        if j.value.count >= k {
+            for z in j.value {
+                count[idx[z]!] += 1
             }
         }
-        if date[0] < todayTime[0] {
-            result.append(i + 1)
-            continue
-        } else if date[0] == todayTime[0] {
-            if date[1] < todayTime[1] {
-                result.append(i + 1)
-                continue
-            } else if date[1] == todayTime[1] {
-                if date[2] <= todayTime[2] {
-                    result.append(i+1)
-                    continue
-                }
-            }
-        }
-        
-
     }
+    print("count",count)
+    return count
     
-    return result
 }
 
-
-let result = solution("2022.05.19", ["A 6", "B 12", "C 3"], ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"])
+let result = solution(["muzi", "frodo", "apeach", "neo"], ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"], 2)
 print(result)
 
+/*
+ 코드 실행은 되지만 시간 초과로 인해 실패
+ func solution(_ id_list:[String], _ report:[String], _ k:Int) -> [Int] {
+     
+     var id_list = id_list
+     print("id_list",id_list)
+     
+     var report = report
+     print("report",report)
+     var result: [Int] = []
+     
+     var reportCom = Array(Set(report.map { $0.components(separatedBy: " ")}))
+     print("reportCom",reportCom)
+     print(type(of: reportCom))
+
+     
+     for i in id_list {
+         var count = 0
+         var empty: [String] = []
+         for j in reportCom {
+             if j[0].contains(i) {
+                 
+                 empty.append(j[1])
+             }
+            
+             
+         }
+         print("\(i):",empty)
+         
+         for z in empty {
+           
+             for j in 0..<reportCom.count {
+                 if z.contains(reportCom[j][1]) {
+                     count += 1
+                 }
+             }
+             print("count",count)
+         }
+         result.append(count/k)
+         print("result",result)
+         
+     }
+     return result
+ }
+ */
+
+/*
+ func solution(_ id_list:[String], _ report:[String], _ k:Int) -> [Int] {
+     
+     var reported_report: [String:Set<String>] = [:]
+     var idIdx:[String:Int] = [:]
+     var count = Array(repeating: 0, count: id_list.count)
+     print("count",count)
+     for (index,id) in id_list.enumerated() {
+             idIdx[id] = index
+     }
+     print("idIdx",idIdx)
+     
+     for r in report {
+         let split = r.split(separator: " ")
+         // split 0번째 인덱스
+         let report = String(split[0])
+         // split 첫번째 인덱스
+         let reported = String(split[1])
+
+         if reported_report[reported] == nil {
+             reported_report[reported] = [report]
+         } else {
+             reported_report[reported]!.insert(report)
+         }
+        
+     }
+     
+     print("reported_report 최종 ",reported_report)
+     
+     for r in reported_report {
+             if r.value.count >= k {
+                 for i in r.value {
+                     count[idIdx[i]!] += 1
+                 }
+             }
+         }
+     
+     return count
+ }
+ */
