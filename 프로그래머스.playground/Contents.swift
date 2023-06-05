b8337861a3be2a3861336af83c50ffdbeef254a9
yeomsungpil <present7256@gmail.com>
2023-06-06 00:27:11 +0900

    이코테: 왕실나이트 다른 풀이 추가

--- 프로그래머스.playground/Contents.swift
+++ 프로그래머스.playground/Contents.swift
@@ -1,26 +1,40 @@
 import Foundation
 
-func solution(_ N: Int) -> Int {
+func solution(_ N: String) -> Int {
+   // 딕셔너리 형태로 만들기
     
-    // 00분 60초일때 3이 몇개인지 먼저 구하면 쉽지 않을까?
+    var rowDic: [String:Int] = ["a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8]
     
+    print(rowDic)
+    
+    var startPoint: [Int] = []
+    let distance = [[-2,1], [-2,-1], [2,-1], [2,1], [-1,2], [-1,-2], [1,-2], [1,2]]
     var count = 0
-   
-    for k in 0...5 {
-        for i in 0...59 {
-            for j in 0...59 {
-                if (String(k) + String(i) + String(j)).contains("3") {
-                    count += 1
-                }
-            }
+    
+ 
+    for i in N {
+        if let number = rowDic[String(i)] {
+            startPoint.append(number)
+        } else {
+            startPoint.append(Int(String(i))!)
         }
     }
-
     
-    print(count)
+    print("startPoint",startPoint)
+
+    for j in 0..<distance.count {
+        
+        var row = startPoint[0] + distance[j][0]
+        var columns = startPoint[1] + distance[j][1]
+        
+        if row > 0 && row < 9 && columns > 0 && columns < 9 {
+            count += 1
+        }
+    }
+    print("count",count)
     
     return 0
 }
 
-let result = solution(5)
+let result = solution("c2")
 print(result)
--- 프로그래머스.playground/Sources/이코테 문제/구현/왕실나이트.swift
+++ 프로그래머스.playground/Sources/이코테 문제/구현/왕실나이트.swift
@@ -49,3 +49,41 @@ func solution2_3(_ n: String) -> Int {
     return count
     
 }
+/*
+ 2023.06.05 다른 풀이
+ func solution(_ N: String) -> Int {
+    // 딕셔너리 형태로 만들기
+     
+     var rowDic: [String:Int] = ["a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8]
+     
+     print(rowDic)
+     
+     var startPoint: [Int] = []
+     let distance = [[-2,1], [-2,-1], [2,-1], [2,1], [-1,2], [-1,-2], [1,-2], [1,2]]
+     var count = 0
+     
+  
+     for i in N {
+         if let number = rowDic[String(i)] {
+             startPoint.append(number)
+         } else {
+             startPoint.append(Int(String(i))!)
+         }
+     }
+     
+     print("startPoint",startPoint)
+
+     for j in 0..<distance.count {
+         
+         var row = startPoint[0] + distance[j][0]
+         var columns = startPoint[1] + distance[j][1]
+         
+         if row > 0 && row < 9 && columns > 0 && columns < 9 {
+             count += 1
+         }
+     }
+     print("count",count)
+     
+     return 0
+ }
+ */
