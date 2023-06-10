0029bf76a3674b6706d4b9e90977e75d61e823c2
yeomsungpil <present7256@gmail.com>
2023-06-10 18:52:52 +0900

    H-Index : 문제 풀이 완료

--- 프로그래머스.playground/Sources/프로그래머스 2단계/H-Index/H-Index.swift
+++ 프로그래머스.playground/Sources/프로그래머스 2단계/H-Index/H-Index.swift
@@ -1,14 +1,16 @@
 import Foundation
 
-func solution(_ citations:[Int]) -> Int {
+func solutionHIndex(_ citations:[Int]) -> Int {
     
     var result: Int = 0
     let sorted = citations.sorted{$0 > $1}
 
+    // 배열의 원소들이 배열의 count(논문의 편수)보다 전부 클 경우엔 count로 바로 리턴하도록 해준다.
     if sorted.min()! > sorted.count {
         result = sorted.count
     } else {
         for i in 0..<sorted.count {
+            // 0부터시작하면 어차피 모든 배열요소의 수만큼 되기때문에 의미가없죠
             if i+1 > sorted[i] {
                 result = i
                 break
@@ -17,5 +19,3 @@ func solution(_ citations:[Int]) -> Int {
     }
     return result
 }
-let result = solution([3, 0, 6, 1, 5])
-print(result)
